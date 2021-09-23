57558cb169defd7d94b35534d2e820e71deecb83
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2021-09-23 15:54:47 +0900

    first commit

---
+++ src/components/Calc.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="card alert-primary">
+    <div class="card-body text-left">
+      <h2 class="card-title text-center">{{ title }}</h2>
+      <p class="card-text h5">{{ message }}</p>
+    </div>
+  </div>
+</template>
